8dadbe5fc1bb4713462f57fcf204620e1b7ed4c9
jason hu <jhu93113@gmail.com>
2021-12-18 16:01:06 -0800

    added test code for odometry

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/OdometryTest.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.util;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.HyperBot;
+
+
+@TeleOp(group = "drive")
+public class OdometryTest extends LinearOpMode {
+    HyperBot robot = new HyperBot();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot.init(hardwareMap);
+        robot.lowerOdo();
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            robot.getCurrentPosition();
+        }
+    }
+}
